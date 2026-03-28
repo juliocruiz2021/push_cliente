cef011b22f63675a60fa9cd5f5863228a57b9535
Julio <juliocruiz2021@github.com>
2026-03-28 11:05:44 -0600

    Disable Sanctum stateful middleware for token API

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -18,7 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
-        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
